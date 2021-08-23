a9cffbf690836fd986142d5bc8f51e65c4b6fecb
Rahul Dhiman <rahul.dhiman@appinventiv.com>
2021-08-23 16:31:56 +0530

    updated App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Modal, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import {Calendar, LocaleConfig} from 'react-native-calender'
 
-
+const DAYS_INTERVAL = 7;
 const App = () => {
   const [startDate, setStartDate] = React.useState();
   const [endDate, setEndDate] = React.useState();
@@ -12,265 +12,121 @@ const App = () => {
   let dateInterval = {};
 
   React.useEffect(()=>{},[interval]);
-  //leap Year function
-  const leapYear = (year, month, day) =>{
-    for( let i=0 ; i<7 ; i++){
-      //if day.day is less than 29
-      if(day <29)
-      {
-      //starting date
-      if(i === 0)
-      {
-        setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //end date
-      else if(i === 6){
-        setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //intermediate dates
-      else{
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
-      }
-    }
-    //when month changes from FEB to MARCH
-    else{
-      //reset the day to 1 for next month
-      if(day === 30)
-      {day = 1; month = month+1};
-      //starting date
-      if(i === 0)
-      {
-        setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
-        
-      }
-      //end date
-      else if(i === 6){
-        setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //intermediate dates
-      else{
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
-      }
-    }
 
-    setInterval(dateInterval);
-    dateInterval={};
-   }
+  //intervalCreater function
+  const intervalCreater = (year, month, day, CHANGE_DECIDER, MIN_DATE ) =>{
+      for(let i=0; i<DAYS_INTERVAL ; i++){
+
+        if(day < MIN_DATE){
+          //starting date
+          if(i === 0)
+          {
+            setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
+            dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
+          }
+          //end date
+          else if(i === 6){
+            setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
+            dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
+          }
+          //intermediate dates
+          else{
+            dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
+          }
+        }
+
+        else{
+          if(day === CHANGE_DECIDER){
+            day = 1; 
+            year =  (month == 12 ? year+1 : year);
+            month = (month == 12 ? 1 : month+1);
+          }
+
+          //starting date
+          if(i === 0){
+            setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
+            dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
+           }
+          //end date
+          else if(i === 6){
+            setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
+            dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
+          }
+          //intermediate dates
+          else{
+            dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
+          }
+        }
+      }
+      setInterval(dateInterval);
+        console.log(dateInterval);
+        dateInterval={};
   }
 
-  //notLeap function
-  const notLeapYear = (year, month, day) =>{
-    for( let i=0 ; i<7 ; i++){
-      //if day is less than 28
-      if(day < 28)
-      {
-      //starting date
-      if(i === 0)
-      {
-        setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
+  //checkLeap function
+  const checkLeap = (year) =>{
+      /* checking for leap year */
+      if (year % 400 == 0) {
+        return 'LEAP_YEAR';
       }
-      //end date
-      else if(i === 6){
-        setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //intermediate dates
-      else{
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
-      }
-    }
-    //when month changes from FEB to MARCH
-    else{
-      //reset the day to 1 for next month
-      if(day === 29)
-      {day = 1; month = month+1};
-      //starting date
-      if(i === 0)
-      {
-        setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //end date
-      else if(i === 6){
-        setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //intermediate dates
-      else{
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
-      }
-    }
-   }
-   setInterval(dateInterval);
-   dateInterval={};
-  }
-
-  //setInterval_30
-  const setInterval_30 = (year, month, day) =>{
-    console.log("MONTH WITH 30 DAYS : ",day)
-    for(let i=0; i<7 ;i++){
-      if(day <= 24)
-      {
-      //starting date
-      if(i === 0)
-      {
-        setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //end date
-      else if(i === 6){
-        setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //intermediate dates
-      else{
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
-      }
-    }
-    //when month changes
-    else{
-      //reset the day to 1 for next month
-      if(day === 31)
-      {day = 1; month = month+1};
-      //starting date
-      if(i === 0)
-      {
-        setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //end date
-      else if(i === 6){
-        setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //intermediate dates
-      else{
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
-      }
-    }
-
-    }
-    setInterval(dateInterval);
-    console.log(interval);
-    dateInterval={};
-  }
-
-  //setInterval_31 function
-  const setInterval_31 = (year, month, day) =>{
-    console.log("MONTH WITH 31 DAYS : ",day)
-    
-    for(let i=0; i<7 ;i++){
-      if(day <= 25)
-      {
-      //starting date
-      if(i === 0)
-      {
-        setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //end date
-      else if(i === 6){
-        setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //intermediate dates
-      else{
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
-      }
-    }
-    //when month changes
-    else{
-      //reset the day to 1 for next month
-      if(day === 32)
-      {
-        day = 1; 
-        year =  (month == 12 ? year+1 : year);
-        month = (month == 12 ? 1 : month+1);
-      };
-      //starting date
-      if(i === 0)
-      {
-        setStartDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {startingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //end date
-      else if(i === 6){
-        setEndDate(`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day : day}`);
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {endingDay: true, color: '#50cebb', textColor: 'white'};
-      }
-      //intermediate dates
-      else{
-        dateInterval[`${year}-${month>9 ? month : `0`+month}-${day<10 ? `0`+day++ : day++}`] = {color: '#70d7c7', textColor: 'white'};
-      }
-    }
-
-    }
-    setInterval(dateInterval);
-    console.log(interval);
-    dateInterval={};
-  }
+      /* not a leap year if divisible by 100
+         but not divisible by 400 */
+     else if (year % 100 == 0) {
+      return 'NOT_LEAP_YEAR';
+     }
+     /* leap year if not divisible by 100
+        but divisible by 4 */
+     else if (year % 4 == 0) {
+      return 'LEAP_YEAR';
+     }
+     /* all other years are not leap years */
+     else {
+       return 'NOT_LEAP_YEAR';
+     }
+  };
 
   //onDayPress
   const onDayPress = (DAY) =>{
-  
     console.log("Day :", DAY.dateString);
     let year = DAY.year;
     let day = DAY.day;
     let month = DAY.month;
    
-    //checking if selected date is on SUNDAY
+    /* checking if selected date is on SUNDAY */
     const Date = new window.Date(DAY.dateString);
     if(Date.getDay() === 0){
-    
-    //if month is FEBURARY
+    /* if month is FEBURARY */
     if(month === 2 ){
-     //checking for leap year
-     if (year % 400 == 0) {
-       //is leap year 
-       leapYear(year, month, day);
-       console.log("INTERVAL ARRAY Leap : ", interval);
-     }
-     // not a leap year if divisible by 100
-    // but not divisible by 400
-    else if (year % 100 == 0) {
-      //not a leap year
-      notLeapYear(year, month, day);
-      console.log("INTERVAL ARRAY NotLeap: ", interval);
-     
-    }
-    // leap year if not divisible by 100
-    // but divisible by 4
-    else if (year % 4 == 0) {
-       //leap year
-       leapYear(year, month, day);
-       console.log("INTERVAL ARRAY LEAP: ", interval);
-    }
-    // all other years are not leap years
-    else {
-       notLeapYear(year,month, day);
-       console.log("INTERVAL ARRAY Not Leap: ", interval);
-    }}
+      const result = checkLeap(year);
+      if (result === 'LEAP_YEAR'){
+        const CHANGE_DECIDER = 30;
+        const MIN_DATE = CHANGE_DECIDER - DAYS_INTERVAL + 1;
+        return intervalCreater(year, month, day, CHANGE_DECIDER, MIN_DATE );
+      }
+      else{
+        const CHANGE_DECIDER = 29;
+        const MIN_DATE = CHANGE_DECIDER - DAYS_INTERVAL + 1;
+        return intervalCreater(year, month, day, CHANGE_DECIDER, MIN_DATE);
+      }
+   }
 
-    //if selected date is from 31 days month
+    /* if selected date is from 31 days month */
     else if( (month <=7 && month%2 !== 0) || (month>=8 && month%2 === 0))
     {
-      setInterval_31(year, month, day);
+      const CHANGE_DECIDER = 32;
+      const MIN_DATE = CHANGE_DECIDER - DAYS_INTERVAL + 1;
+      return intervalCreater(year, month, day, CHANGE_DECIDER, MIN_DATE);
     }
-    // months with 30 days
+    /* months with 30 days */
     else {
-      setInterval_30(year, month, day);
-    }
-   }
+      const CHANGE_DECIDER = 31;
+      const MIN_DATE = CHANGE_DECIDER - DAYS_INTERVAL + 1;
+      return intervalCreater(year, month, day, CHANGE_DECIDER, MIN_DATE);
+    }}
+
    else{
      console.log("SELECT A DATE THAT IS ON SUNDAY");
-   }
-
-   }
+   }};
 
   return (
     <SafeAreaView style={styles.mainView}>
